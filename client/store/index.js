a622e98f152b238e637e90c13141e84d67df8600
Tess Monical <tess.monical@gmail.com>
2017-09-22 14:25:25 -0500

    use redux devtools

--- client/store/index.js
+++ client/store/index.js
@@ -8,8 +8,16 @@ const reducer = combineReducers({
   user,
   adventures
 })
-const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-const store = createStore(reducer, middleware)
+
+
+//SET w/ REDUX DevTools
+const middleware = [thunkMiddleware, createLogger({collapsed: true})]
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(...middleware)
+  ));
+
 
 export default store
 export * from './user'
