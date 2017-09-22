59127fe5937c8468ba13161fe73df172cddf0be0
Tess Monical <tess.monical@gmail.com>
2017-09-22 11:08:55 -0500

    updated adventure api for better error handling

--- server/api/adventures.js
+++ server/api/adventures.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {Adventure, Passage } = require('../db/models')
+const { Adventure, Passage } = require('../db/models')
 module.exports = router
 
 router.get('/', (req, res, next) => {
@@ -10,32 +10,45 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   Adventure.findById(Number(req.params.id))
-  .then(adventure => adventure.getPassages())
-  .catch(next)
+    .then(adventure => adventure.getPassages())
+    .catch(next)
 })
 
 router.get('/passages/:id', (req, res, next) => {
   Adventure.findById(Number(req.params.id))
-  .then(adventure => adventure.getPassages())
-  .catch(next)
+    .then((adventure) => {
+      if (!adventure) res.status(404).send('no such adventure')
+      else return adventure.getPassages()
+    })
+    .then(passages => res.send(passages))
+    .catch(next)
 })
 
 router.get('/:id', (req, res, next) => {
   Adventure.findById(Number(req.params.id))
-    .then(adventure => res.json(adventure))
+  .then((adventure) => {
+    if (!adventure) res.status(404).send('no such adventure');
+    else res.json(adventure);
+  })
     .catch(next)
 })
 
 router.put('/:id', (req, res, next) => {
   Adventure.findById(Number(req.params.id))
-  .then( adventure => adventure.update(req.body))
-  .then(adventure => res.json(adventure))
-  .catch(next)
+    .then(adventure => {
+      if (!adventure) res.status(404).send('no such adventure');
+      else return adventure.update(req.body)
+    })
+    .then(adventure => res.json(adventure))
+    .catch(next)
 })
 
 router.delete('/:id', (req, res, next) => {
   Adventure.findById(Number(req.params.id))
-  .then( adventure => adventure.delete(req.body))
-  .then( () => res.sendStatus(204))
-  .catch(next)
+    .then(adventure => {
+      if (!adventure) res.status(404).send('no such adventure')
+      else return adventure.delete(req.body)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
 })
