b33deb932962c24ba8f00c8ccf6191aa65b4eee3
Tess Monical <tess.monical@gmail.com>
2017-09-23 15:37:33 -0500

    adds create passage form

---
+++ client/components/create-passage-form.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import { createPassage } from '../store';
+
+class CreatePassageForm extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      description: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event, { name, value }) {
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+    this.props.createAdventure(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>New Passage</h2>
+        <Form onSubmit={this.handleSubmit}>
+          <Input label="Title" type="text" name="title" onChange={this.handleChange} value={this.state.title} />
+          <TextArea label="Description" name="description" style={{ minHeight: 100 }} onChange={this.handleChange} />
+          <Button basic color="green" type="submit">Create</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+const mapState = (state) => {
+  return {}
+}
+
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    createPassage: (passage) => {
+      const adventureId = Number(ownProps.match.params.id);
+      newPassage = { ...passage, adventureId };
+      dispatch(createPassage(newPassage));
+    }
+  }
+}
+
+export default connect(mapState, mapDispatch)(CreatePassageForm);
---
+++ client/store/passages.js
@@ -0,0 +1,106 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_PASSAGE = 'GET_PASSAGE';
+const GET_ALL_PASSAGES = 'GET_ALL_PASSAGES';
+const REMOVE_PASSAGE = 'REMOVE_PASSAGE';
+const MODIFY_PASSAGE = 'MODIFY_PASSAGE';
+const GET_SOME_PASSAGES = 'GET_SOME_PASSAGES'
+
+/**
+ * INITIAL STATE
+ */
+const defaultPassages = [];
+
+/**
+ * ACTION CREATORS
+ */
+export const getPassage = passage => ({ type: GET_PASSAGE, passage })
+export const modifyPassage = passage => ({ type: MODIFY_PASSAGE, passage })
+export const removePassage = passage => ({ type: REMOVE_PASSAGE, passage })
+export const getAllPassages = passages => ({ type: GET_ALL_PASSAGES, passages })
+export const getSomePassages = passages => ({ type: GET_SOME_PASSAGES, passages })
+
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchPassage = (id) =>
+  dispatch =>
+    axios.get(`/api/passages/${id}`)
+      .then(res => {
+        if (res.data) dispatch(getPassage(res.data));
+      })
+      .catch(err => console.log(err))
+
+export const fetchAllPassages = () =>
+  dispatch =>
+    axios.get(`/api/passages/`)
+      .then(res => {
+        if (res.data) dispatch(getAllPassages(res.data));
+      })
+      .catch(err => console.log(err))
+
+export const fetchAdventurePassages = (id) =>
+  dispatch =>
+    axios.get(`/api/adventures/passages/${id}`)
+      .then(res => {
+        if (res.data) dispatch(getSomePassages(res.data));
+      })
+      .catch(err => console.log(err))
+
+export const createPassage = (passage) =>
+  dispatch =>
+    axios.post(`/api/adventures/`, passage)
+      .then(res => {
+        if (res.data) {
+          dispatch(getPassage(res.data));
+          history.push(`/passages/${res.data.id}`)
+        }
+      })
+      .catch(err => console.log(err))
+
+export const editPassage = (passage) =>
+  dispatch =>
+    axios.put(`/api/passages/${passage.id}`, passage)
+      .then(res => {
+        if (res.data) {
+          dispatch(modifyPassage(res.data));
+          history.push(`/passages/${res.data.id}`)
+        }
+      })
+      .catch(err => console.log(err))
+
+export const deletePassage = (id) =>
+  dispatch =>
+    axios.delete(`/api/passage/${id}`)
+      .then(() => {
+        dispatch(removePassage());
+      })
+      .catch(err => console.log(err))
+
+/**
+ * REDUCER
+ */
+export default function (passages = defaultPassages, action) {
+  let index;
+  switch (action.type) {
+    case GET_PASSAGE:
+      return [...passages, action.adventure]
+    case REMOVE_PASSAGE:
+      index = passages.findIndex(passage => passage.id === action.passage.id);
+      return [].concat(passages.slice(0, index), passages.slice(index + 1))
+    case GET_ALL_PASSAGES:
+      return action.passages;
+    case GET_SOME_PASSAGES:
+      return [...passages].concat(action.passages);
+    case MODIFY_PASSAGE:
+      index = passages.findIndex(passage => passage.id === action.passage.id);
+      return [action.passage].concat(passages.slice(0, index), passages.slice(index + 1))
+    default:
+      return passages;
+  }
+}
